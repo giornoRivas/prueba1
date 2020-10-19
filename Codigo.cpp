82b522780b7bf4e787aa81086d5eeee6b5985e4f
Ian Rivas <leondantex@gmail.com>
2020-10-19 09:48:31 -0300

    4to commit

--- Codigo.cpp
+++ Codigo.cpp
@@ -5,11 +5,11 @@ using namespace std;
 
 int main()
 {
-    int x=2 ,y=3;
+    int x=2 ,y=3,z=2;
     cout << "Hola putos"<< endl;
     cout << "Vamos a practicar git"<< endl;
     cout << "seba gato" << endl;
-
+    
     cout << "x="<< x << "and y="<< y <<endl;
 
     return 0;
