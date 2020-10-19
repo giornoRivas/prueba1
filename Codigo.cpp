56f18ebf0cc40e2a44bad36d3ef596bca66e3c65
Ian Rivas <leondantex@gmail.com>
2020-10-19 09:39:49 -0300

    3er commit

--- Codigo.cpp
+++ Codigo.cpp
@@ -5,9 +5,12 @@ using namespace std;
 
 int main()
 {
+    int x=2 ,y=3;
     cout << "Hola putos"<< endl;
     cout << "Vamos a practicar git"<< endl;
     cout << "seba gato" << endl;
-    
+
+    cout << "x="<< x << "and y="<< y <<endl;
+
     return 0;
 }
